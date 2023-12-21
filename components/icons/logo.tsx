33a9981ad18bf3c644d0d695b99aaeab8df47724
Nazarii <nazariikubik@gmail.com>
2023-12-21 03:03:40 -0800

    fix2

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,8 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/alt-text */
 import clsx from 'clsx';
 import Image from 'next/image';
 
 export default function LogoIcon(props: React.ComponentProps<'svg'>) {
   return (
-    <Image src={'/Logo.png'} className={clsx('h-full w-full object-cover p-2', {})} fill={true} />
+    <Image
+      src={'/Logo.png'}
+      className={clsx('h-full w-full object-cover p-2', {})}
+      fill={true}
+      alt={''}
+    />
   );
 }
